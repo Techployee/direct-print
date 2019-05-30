0009d02cd9ef87209f13441f7182f903a7a66d36
Martin Iannuzzi <martin@techployee.com>
2019-05-30 13:20:07 +1000

    Added company

--- src/DirectPrint/Properties/AssemblyInfo.cs
+++ src/DirectPrint/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DirectPrint")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Techployee")]
 [assembly: AssemblyProduct("DirectPrint")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
